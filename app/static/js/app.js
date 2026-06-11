24f81659ded68ae0d0122573789f86685f59abd6
Uzalov-Gamid <uzalovgamid@gmail.com>
2026-06-11 18:39:09 +0300

    style: улучшить адаптивный интерфейс

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,13 @@
 document.documentElement.classList.add('js-ready');
+
+document.querySelectorAll('.status-form select').forEach((select) => {
+    select.addEventListener('change', () => {
+        select.closest('.status-form')?.classList.add('is-dirty');
+    });
+});
+
+document.querySelectorAll('form').forEach((form) => {
+    form.addEventListener('submit', () => {
+        form.classList.add('is-submitting');
+    });
+});
